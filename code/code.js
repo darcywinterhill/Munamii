9dc0f46aa3180568d09ae2b5facb74185c2edb54
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2023-12-01 12:57:34 +0100

    added footer w/ links, to-top-button, mobile view working

--- code/code.js
+++ code/code.js
@@ -1,3 +1,20 @@
+let arrowButton = document.getElementById("arrow-button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    arrowButton.style.display = "block";
+  } else {
+    arrowButton.style.display = "none";
+  }
+}
+
+function toTopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 function loadCakes() {
   let result = "";
 
