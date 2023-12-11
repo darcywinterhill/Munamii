23ad2a47e21d169747303b021dd8a1f5d8b5254e
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2023-12-11 15:54:11 +0100

    trying a function for vh

--- code/code.js
+++ code/code.js
@@ -1,3 +1,7 @@
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 /* SCROLL TO TOP BUTTON */
 let arrowButton = document.getElementById("arrow-button");
